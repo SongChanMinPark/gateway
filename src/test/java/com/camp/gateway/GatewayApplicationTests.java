a777b6e69d65549a67c035a0910bbed69331ca4a
ilsongpark <darbysong@naver.com>
2023-08-11 19:28:22 +0900

    테스트 제거

--- src/test/java/com/camp/gateway/GatewayApplicationTests.java
+++ src/test/java/com/camp/gateway/GatewayApplicationTests.java
@@ -3,10 +3,10 @@ package com.camp.gateway;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class GatewayApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
